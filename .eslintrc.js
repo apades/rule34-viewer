a31f6a8d8ac03c2bbc570cdf0db5e94ce7f47d06
apades <apad2@qq.com>
2020-11-03 12:10:21 +0800

    增加eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    commonjs: true,
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  ignorePatterns: ["node_modules"],
+  extends: ['react-app', 'prettier'],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    'no-console': 'off',
+    'prettier/prettier': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'no-plusplus': 'off',
+    'require-await': 'off',
+    'no-unused-vars': 'off',
+    'no-useless-escape': 'off',
+    'no-proto': 'off',
+    // 链式判断
+    'no-unused-expressions': 'off',
+    camelcase: 0,
+    'react/jsx-sort-props': 1,
+    'react-hooks/exhaustive-deps': 0,
+  },
+}

77557e0fcff38392d07c9ea5f04e829f20339d18
apades <apad2@qq.com>
2020-11-06 16:03:39 +0800

    增加gallery header

---
+++ views/gallery/header.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { IconButton } from 'react-native-paper'
+import { connect } from 'react-redux'
+
+function dom({ getLike, tag, likesToggle }) {
+  //   let [like, setLike] = useState(getLike(tag))
+  let like = getLike(tag)
+
+  let styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+    },
+  })
+  return (
+    <View style={styles.container}>
+      <Text>{tag}</Text>
+      <IconButton
+        color="#6cf"
+        icon={like ? 'heart' : 'heart-outline'}
+        onPress={() => {
+          likesToggle(tag)
+          //   setLike(!like)
+        }}
+        size={15}
+      />
+    </View>
+  )
+}
+
+let GalleryHeader = connect(
+  (state) => ({
+    getLike: (tag) => state.likes.tags[tag],
+  }),
+  (dispatch) => ({
+    likesToggle: (tag) => dispatch({ type: 'likes/tag_toggle', tag }),
+  }),
+)(dom)
+
+export default GalleryHeader

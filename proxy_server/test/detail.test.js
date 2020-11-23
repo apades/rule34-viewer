c48e8eaf6b3a5c535583f5abb8de8bcf5df70de7
apades <apad2@qq.com>
2020-11-23 16:32:39 +0800

    new test

--- proxy_server/test/detail.test.js
+++ proxy_server/test/detail.test.js
@@ -6,30 +6,6 @@ let html = readFileSync(`${__dirname}\\detail.html`, 'utf-8')
 
 // console.log(resolveData(html))
 
-let test = `<!DOCTYPE html>
-  <html lang="en">
-  
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-  </head>
-  
-  <body>
-  <ul class="flat-list" id="subnavbar" style="margin-bottom: 1px;">
-  <li><a href="index.php?page=post&amp;s=list">List</a></li>
-  <li><a href="index.php?page=post&amp;s=add">Upload</a></li>
-  <li><a href="index.php?page=post&amp;s=addVideo">Upload WebM</a></li>
-  <li><a href="index.php?page=post&amp;s=random">Random</a></li>
-  <li><a href="mailto:staff@booru.org">Contact Us</a></li>
-  <li><a href="mailto:dmca@booru.org">DMCA</a></li>
-  <li><a href="index.php?page=about">About</a></li>
-  <li><a href="index.php?page=help&amp;topic=post">Help</a></li>
-  <li><a href="index.php?page=servermap">IMG Servers</a></li>
-  <li><a href="index.php?page=toptags">TagMap</a></li>
-  <li><a href="/tos.php">TOS</a></li>
-</ul>
-</body>`
-const root = parse(test)
-let els = root.querySelectorAll('.flat-list a')
+const root = parse(html)
+let els = root.querySelectorAll('.tag-type-copyright a')
 console.log(els.map((el) => el.text))

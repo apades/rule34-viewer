93bb680ab4436b051ac834c57c23d173c09adbef
apades <apad2@qq.com>
2020-11-24 15:42:11 +0800

    增加了个layout和detail增改

--- views/detail/index.js
+++ views/detail/index.js
@@ -1,49 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AutoHeightImage from 'react-native-auto-height-image'
 import { Image, View } from 'react-native'
 import { FAB, Text } from 'react-native-paper'
 import { connect } from 'react-redux'
 import { _env, _screen } from '../../utils/env'
+import statuBarLayout from '../../layout/statuBar'
 
 const Detail = connect((state) => ({
   dataList: state.imgList.dataList,
   count: state.imgList.count,
   pid: state.imgList.pid,
   index: state.imgList.index,
-  getLikes: (img) => state.likes.imgs[img],
+  getLikes: (id) => state.likes.imgs[id],
 }))(function (props) {
-  let { navigation, route } = props
+  let { navigation, route, dispatch } = props
 
   let { dataList, index } = props
-  let uri = dataList[index].file_url
+  let data = dataList[index]
+  let uri = data.file_url
   let ImageEl = _env.NSFW ? (
     <AutoHeightImage source={{ uri }} width={_screen.width} />
   ) : (
     <Text>img:{uri}</Text>
   )
 
-  let FabEl = (
-    <FAB
-      icon="heart"
-      onPress={() => {
-        console.log('like!')
-      }}
-      style={{
-        position: 'absolute',
-        bottom: 10,
-        right: 10,
-      }}
-    />
-  )
+  function RenderLike() {
+    let { getLikes } = props
+    let [like, setLike] = useState(getLikes(data.id))
+    return (
+      <FAB
+        icon={like ? 'heart' : 'heart-outline'}
+        onPress={() => {
+          console.log(`like!,${data.id}`)
+          dispatch({ type: 'likes/img_toggle', id: data.id, data })
+          setLike(!like)
+        }}
+        style={{
+          position: 'absolute',
+          bottom: 10,
+          right: 10,
+        }}
+      />
+    )
+  }
 
-  return (
-    <View style={{ flex: 1, position: 'relative', flexDirection: 'column' }}>
-      {ImageEl}
-      <View>
-        <Text>this is tags container</Text>
-      </View>
-      {FabEl}
-    </View>
-  )
+  return statuBarLayout({
+    style: { position: 'relative' },
+    children: () => (
+      <>
+        {ImageEl}
+        <View>
+          <Text>this is tags container</Text>
+        </View>
+        {RenderLike()}
+      </>
+    ),
+  })
 })
 export default Detail

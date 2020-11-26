4760ed1c08bda60d57dcd53e33626deb75c4719c
apades <apad2@qq.com>
2020-11-26 11:07:50 +0800

    更新文档

--- config/base.config.js
+++ config/base.config.js
@@ -3,6 +3,23 @@ const config = {
   gallery: {
     rowNum: 2,
     pageNum: 20,
+    request: {
+      gallery: {
+        url:
+          'https://rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&tags=${tags}&limit=${pageNum}&pid=${page}',
+        img:
+          'https://rule34xxx/thumbnails/${item.directory}/thumbnails_${item.image}',
+      },
+      detail: {
+        url: '',
+        img: 'https://rule34xxx/images/${item.directory}/${item.image}',
+        tags: {
+          key: '',
+          callback: 'fn',
+          reg: '',
+        },
+      },
+    },
   },
   showDebugComponent: false,
 }

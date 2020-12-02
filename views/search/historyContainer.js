b5b1e414525af0f1ef48e1510c4ca98c32f65a58
apades <apad2@qq.com>
2020-12-02 13:40:14 +0800

    优化searchHistoryContainer闪动的问题

---
+++ views/search/historyContainer.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import { ScrollView, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { Text } from 'react-native-paper'
+import { connect } from 'react-redux'
+import { searchCompleteList } from '../../api/list_o'
+import { _style } from '../../style'
+import { debounceAsync } from '../../utils/utils'
+
+let fn = debounceAsync(async (text = '') => {
+  let searchArr = text.trim().split(' ')
+  let searchDataList = []
+  if (text.trim().length) {
+    let nowCompelte = searchArr[searchArr.length - 1]
+    searchDataList = await searchCompleteList(nowCompelte)
+  }
+
+  return searchDataList
+}, 500)
+
+let SearchHistoryContainer = connect((state) => ({
+  histories: state.search.histories,
+}))((props) => {
+  let [dataList, setDataList] = useState([])
+
+  let { text, setText } = props
+
+  useEffect(() => {
+    try {
+      fn(text).then(
+        (rs) => {
+          if (text.trim().length) setDataList(rs)
+          else setDataList(props.histories)
+        },
+        () => ({}),
+      )
+    } catch (error) {}
+  }, [text])
+
+  return (
+    <ScrollView>
+      {dataList.map((data, index) => {
+        let { value = data, label = data } = data
+
+        return (
+          <TouchableOpacity
+            key={index}
+            onPress={() => {
+              let searchArr = text.trim().split(' ') || []
+              searchArr[searchArr.length - 1] = value
+              setText(searchArr.join(' '))
+            }}
+          >
+            <View style={{ padding: 10, ..._style.center, ..._style.border() }}>
+              <Text>{label}</Text>
+            </View>
+          </TouchableOpacity>
+        )
+      })}
+    </ScrollView>
+  )
+})
+
+export default SearchHistoryContainer
--- views/search/index.js
+++ views/search/index.js
@@ -1,14 +1,9 @@
-import { useNavigation } from '@react-navigation/native'
-import { debounce } from 'lodash'
-import React, { useEffect, useState } from 'react'
-import { ScrollView, View } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import { Text } from 'react-native-paper'
-import { connect } from 'react-redux'
+import React, { useState } from 'react'
+import { View } from 'react-native'
 import { searchCompleteList } from '../../api/list_o'
-import { Comp_seachInput } from '../../components/searchInput'
 import { _style } from '../../style'
 import { debounceAsync } from '../../utils/utils'
+import SearchHistoryContainer from './historyContainer'
 import SearchInputEl from './searchInputEl'
 
 let fn = debounceAsync(async (text = '') => {
@@ -27,56 +22,13 @@ export default function Search(props) {
 
   let [text, setText] = useState(props.value || '')
 
-  let SearchHistoryContainer = connect((state) => ({
-    histories: state.search.histories,
-  }))((props) => {
-    let [dataList, setDataList] = useState([])
-
-    useEffect(() => {
-      try {
-        fn(text).then(
-          (rs) => {
-            if (text.trim().length) setDataList(rs)
-            else setDataList(props.histories)
-          },
-          () => ({}),
-        )
-      } catch (error) {}
-    }, [text])
-
-    return (
-      <ScrollView>
-        {dataList.map((data, index) => {
-          let { value = data, label = data } = data
-
-          return (
-            <TouchableOpacity
-              key={index}
-              onPress={() => {
-                let searchArr = text.trim().split(' ') || []
-                searchArr[searchArr.length - 1] = value
-                setText(searchArr.join(' '))
-              }}
-            >
-              <View
-                style={{ padding: 10, ..._style.center, ..._style.border() }}
-              >
-                <Text>{label}</Text>
-              </View>
-            </TouchableOpacity>
-          )
-        })}
-      </ScrollView>
-    )
-  })
-
   return (
     <View style={{ flex: 1, ..._style.center() }}>
       <View style={{ width: '80%' }}>
         {/* {searchInputEl} */}
         <SearchInputEl navigation={navigation} setText={setText} text={text} />
         <View style={{ marginTop: 10 }}>
-          <SearchHistoryContainer />
+          <SearchHistoryContainer setText={setText} text={text} />
         </View>
       </View>
     </View>

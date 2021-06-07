2800e98f934e1bdfb94dee7a36510d3bff5a7272
apades <apad2@qq.com>
2021-06-07 15:18:25 +0800

    [feat] commit script

--- config/router.confg.js
+++
@@ -1,6 +0,0 @@
-var router = {
-  path: '',
-  component: '',
-}
-
-export default []
---
+++ script/comment.js
@@ -0,0 +1,33 @@
+let inq = require('inquirer')
+const { execSync } = require('child_process')
+
+inq
+  .prompt([
+    {
+      name: 'ctype',
+      message: 'select type',
+      choices: [
+        '[chore]',
+        '[fix]',
+        '[feat]',
+        '[ui]',
+        '[core]',
+        '[rewrite]',
+        '[update]',
+        '[test]',
+      ],
+      type: 'list',
+    },
+    {
+      name: 'comment',
+      message: 'comment:',
+      type: 'input',
+    },
+  ])
+  .then((res) => {
+    try {
+      execSync(`git add . && git commit -am ${res['ctype']} ${res['comment']}`)
+    } catch (error) {
+      console.log(error.toString())
+    }
+  })

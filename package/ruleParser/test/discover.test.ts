dc41ebc1899d780de3d6ca5e5855954ef38215e6
apades <apad2@qq.com>
2021-06-09 16:34:21 +0800

    [feat] add auto rule auto test framework

--- package/ruleParser/test/discover.test.ts
+++ package/ruleParser/test/discover.test.ts
@@ -2,24 +2,39 @@ import getRuleResult, { setRule } from '..'
 import { readFileSync } from 'fs'
 import { resolve } from 'path'
 
-console.time('read rule')
-let ruleText = readFileSync(resolve(__dirname, '../rules/rule34.js'), 'utf-8')
-let _setRule = setRule
-eval(`${ruleText};_setRule(config)`)
-console.timeEnd('read rule')
-// ---
-;(async function () {
-  console.time('eval')
-  let list = await getRuleResult('discover.url', {
-    searchString: 'dacad',
-    pageNum: 0,
-    pageLimit: 10,
+beforeAll(() => {
+  let ruleText = readFileSync(resolve(__dirname, '../rules/rule34.js'), 'utf-8')
+  let _setRule = setRule
+  eval(`${ruleText};_setRule(config)`)
+})
+describe('discover 规则测试', () => {
+  let list: any[] = []
+  beforeAll(async () => {
+    list = await getRuleResult('discover.list', {
+      searchString: 'dacad',
+      pageNum: 0,
+      pageLimit: 10,
+    })
   })
-  console.log('list.length', list)
+  test('discover.url', () => {
+    return getRuleResult('discover.url', {
+      searchString: 'dacad',
+      pageNum: 0,
+      pageLimit: 10,
+    }).then((url) => expect(url).toMatch(/https?:\/\//))
+  })
 
-  let cover = await getRuleResult('discover.cover', {
-    $item: list[0],
+  test('discover.list', () => {
+    return getRuleResult('discover.list', {
+      searchString: 'dacad',
+      pageNum: 0,
+      pageLimit: 10,
+    }).then((l) => expect(l.length).toBe(10))
   })
-  console.log('cover', cover)
-  console.timeEnd('eval')
-})()
+
+  test('discover.cover', () => {
+    return getRuleResult('discover.cover', {
+      $item: list[0],
+    }).then((cover) => expect(cover).toMatch(/https?:\/\//))
+  })
+})

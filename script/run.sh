def81a9d91c769bd755d454a91bb9d90073b2429
apades <apad2@qq.com>
2021-06-15 22:04:45 +0800

    [core] 启动脚本更新

--- script/run.sh
+++ script/run.sh
@@ -1,4 +1,4 @@
 LIP=$(ipconfig.exe | grep -a -E -o '192\.168\.[0-9]{1,3}\.(([2-9][0-9]{0,2})|([0-9]{2,3}))')
 echo Local_ip:${LIP}
 
-cross-env NSFW=false LOCALIP=${LIP} react-native run-android
+cross-env NSFW=$1 LOCALIP=${LIP} react-native run-android
